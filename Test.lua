9c5de5624007013928d7d16cb1a35ae17c4aa779
Freemaker <ralf.rose.oskar@gmail.com>
2024-05-17 09:32:38 +0200

    sync

--- Test.lua
+++ Test.lua
@@ -1,2 +1,3 @@
-local FileSystem, err = package.loadlib("C:\\Coding\\Lua\\Utils\\fileSystem\\cmake-build-release\\libfilesystem.dll", "luaopen_libfilesystem", "returnerror")
+local FileSystem, err = package.loadlib("C:\\Coding\\Lua\\Utils\\fileSystem\\cmake-build-release\\libfilesystem.dll", "luaopen_libfilesystem")
+print(FileSystem, err)
 print(FileSystem.GetCurrentDirectory())
