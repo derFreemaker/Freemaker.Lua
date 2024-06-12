65b3b0597b54dcf936c56956104bb266eed49a16
Freemaker <ralf.rose.oskar@gmail.com>
2024-06-12 12:46:44 +0200

    remvoed Test.lua

--- Test.lua
+++
@@ -1,3 +0,0 @@
-local FileSystem, err = package.loadlib("C:/Coding/Lua/Utils/fileSystem/cmake-build-release-mingw/libfilesystem.dll", "luaopen_libfilesystem")
-print(FileSystem, err)
-print(FileSystem.GetCurrentDirectory())
