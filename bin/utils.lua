6d2c63b1146e61fdd685c736bd2bae899444738c
Freemaker <ralf.rose.oskar@gmail.com>
2024-11-22 11:35:26 +0100

    bundle commit

--- bin/utils.lua
+++ bin/utils.lua
@@ -419,12 +419,22 @@ __bundler__.__files__["src.utils.value"] = function()
 	---@return T
 	function value.copy(x)
 	    local typeStr = type(x)
-
 	    if typeStr == "table" then
 	        return table.copy(x)
 	    end
 
 	    return x
+	end
+
+	---@generic T
+	---@param value T | nil
+	---@param default_value T
+	---@return T
+	function value.default(value, default_value)
+	    if value == nil then
+	        return default_value
+	    end
+	    return value
 	end
 
 	return value
