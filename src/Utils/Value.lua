cc7adb3d777798849e1881b2d3fcdaf6da33bf67
Freemaker <ralf.rose.oskar@gmail.com>
2024-11-22 11:30:08 +0100

    added value.default

--- src/Utils/Value.lua
+++ src/Utils/Value.lua
@@ -8,12 +8,22 @@ local value = {}
 ---@return T
 function value.copy(x)
     local typeStr = type(x)
-
     if typeStr == "table" then
         return table.copy(x)
     end
 
     return x
+end
+
+---@generic T
+---@param value T | nil
+---@param default_value T
+---@return T
+function value.default(value, default_value)
+    if value == nil then
+        return default_value
+    end
+    return value
 end
 
 return value
