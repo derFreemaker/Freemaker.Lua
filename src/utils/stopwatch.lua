e93d4f96dee23cb1bfc92a32d31c8d1dbb09e9a0
Freemaker <ralf.rose.oskar@gmail.com>
2025-01-30 12:22:48 +0100

    improvment stopwatch

--- src/utils/stopwatch.lua
+++ src/utils/stopwatch.lua
@@ -7,7 +7,8 @@ local _number = require("src.utils.number")
 ---@field end_time number
 ---@field private elapesd_milliseconds integer
 ---
----@field private last_lap_time number | nil
+---@field private laps integer[]
+---@field private laps_count integer
 local _stopwatch = {}
 
 ---@return Freemaker.utils.stopwatch
@@ -18,6 +19,9 @@ function _stopwatch.new()
         start_time = 0,
         end_time = 0,
         elapesd_milliseconds = 0,
+
+        laps = {},
+        laps_count = 0,
     }, { __index = _stopwatch })
 end
 
@@ -50,11 +54,12 @@ function _stopwatch:stop()
 end
 
 ---@return integer
-function _stopwatch:get_elapesd_milliseconds()
-    if self.running then
-        return 0
-    end
+function _stopwatch:get_elapesd_seconds()
+    return _number.round(self.elapesd_milliseconds / 1000)
+end
 
+---@return integer
+function _stopwatch:get_elapesd_milliseconds()
     return self.elapesd_milliseconds
 end
 
@@ -65,12 +70,30 @@ function _stopwatch:lap()
     end
 
     local lap_time = os.clock()
-    local previous_lap = self.last_lap_time or self.start_time
-    self.last_lap_time = lap_time
+    local previous_lap = self.laps[#self.laps_count] or self.start_time
+
+    self.laps_count = self.laps_count + 1
+    self.laps[self.laps_count] = previous_lap
 
     local elapesd_time = lap_time - previous_lap
 
     return _number.round(elapesd_time * 1000)
+end
+
+---@return integer elapesd_milliseconds
+function _stopwatch:avg_lap()
+    if not self.running then
+        return 0
+    end
+
+    self:lap()
+
+    local sum = 0
+    for _, lap_time in ipairs(self.laps) do
+        sum = sum + lap_time
+    end
+
+    return sum / #self.laps
 end
 
 return _stopwatch
